cdaf0969d579cf3a426a25bfbc478ebf2c7e0c5f
duy <duynl2@topica.edu.vn>
2018-08-11 12:46:48 +0700

    revert

--- server.js
+++ server.js
@@ -1,8 +1,4 @@
 var express = require('express');
-var path = require('path');
 var app = express();
-// app.use(express.static(__dirname + '/'));
-app.get('*', (req, res) =>{
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
-app.listen(process.env.PORT || 8081);
+app.use(express.static(__dirname + '/'));
+app.listen(process.env.PORT || 8080);
--- webpack.config.js
+++ webpack.config.js
@@ -9,27 +9,13 @@ module.exports = {
   },
   module: {
     rules: [
-      // {
-      //   test: /\.js$/,
-      //   exclude: /node_modules/,
-      //   loader: 'babel-loader',
-      //   query: {
-      //     presets: ['react', 'es2015', 'react-hmre'],
-      //     plugins: ['transform-class-properties', 'transform-object-rest-spread']
-      //   }
-      // },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          presets: ['react', 'es2015'],
-          plugins: ['transform-class-properties', 'transform-object-rest-spread'],
-          env: {
-            development: {
-                presets: ["react-hmre"]
-            }
-          }
+          presets: ['react', 'es2015', 'react-hmre'],
+          plugins: ['transform-class-properties', 'transform-object-rest-spread']
         }
       },
       {
