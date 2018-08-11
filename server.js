8d1e693b37b33ab447dd056b69b3918774932ce1
duy <duynl2@topica.edu.vn>
2018-08-11 12:26:39 +0700

    resolve file path

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-app.use(express.static(__dirname + '/'));
+// app.use(express.static(__dirname + '/'));
 app.get('*', (req, res) =>{
-    res.sendFile(path.resolve(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 app.listen(process.env.PORT || 8081);
